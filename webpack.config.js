e65badba4deb92de1816ef5629588e2368de83d2
Norbert <kurzawski.n@gmail.com>
2021-02-10 20:33:22 +0100

    Make setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+      port: 3000,
+      contentBase: path.join(__dirname, 'public'),
+      watchContentBase: true
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
